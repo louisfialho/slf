11a85dab0bb4200ae3964fc67be2d341f9b02009
Louis Fialho <louis.fialho@hec.edu>
2020-12-25 11:51:09 +0000

    when deleting a space, all children spaces are deleted

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -54,6 +54,10 @@ before_action :set_space, only: [:show, :edit, :update, :destroy]
   end
 
   def destroy
+    sc = Connection.where(space_id: @space.id).first
+    sc.descendants.reverse.each do |descendant|
+      descendant.space.destroy
+    end
     @space.destroy
     redirect_to root_path #not perfect: should redirect to shelf
   end
