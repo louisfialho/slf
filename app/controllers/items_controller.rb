b28fdb30c1b02072326fc832296ecc102284be89
Louis Fialho <louis.fialho@hec.edu>
2020-12-26 11:50:17 +0000

    added item in space

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,19 +2,25 @@ class ItemsController < ApplicationController
 before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def new
+    @item = Item.new
     if params[:shelf_id].present?
-      @item = Item.new
       @shelf = Shelf.find(params[:shelf_id])
+    elsif params[:space_id].present?
+      @space = Space.find(params[:space_id])
     end
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
     if params[:item][:shelf_id].present?
-      @item = Item.new(item_params)
-      @item.save
       @shelf = Shelf.find(params[:item][:shelf_id])
       @shelf.items << @item
       redirect_to item_path(@item, shelf_id: @shelf.id)
+    elsif params[:item][:space_id].present?
+      @space = Space.find(params[:item][:space_id])
+      @space.items << @item
+      redirect_to item_path(@item, space_id: @space.id)
     end
   end
 
@@ -25,25 +31,39 @@ before_action :set_item, only: [:show, :edit, :update, :destroy]
   def show
     if params[:shelf_id].present?
       @shelf = Shelf.find(params[:shelf_id])
+    elsif params[:space_id].present?
+      @space = Space.find(params[:space_id])
     end
   end
 
   def edit
     if params[:shelf_id].present?
       @shelf = Shelf.find(params[:shelf_id])
+    elsif params[:space_id].present?
+      @space = Space.find(params[:space_id])
     end
   end
 
   def update
     @item.update(item_params)
-    @shelf = Shelf.find(params[:item][:shelf_id])
-    redirect_to item_path(@item, shelf_id: @shelf.id)
+    if params[:item][:shelf_id].present?
+      @shelf = Shelf.find(params[:item][:shelf_id])
+      redirect_to item_path(@item, shelf_id: @shelf.id)
+    elsif params[:item][:space_id].present?
+      @space = Space.find(params[:item][:space_id])
+      redirect_to item_path(@item, space_id: @space.id)
+    end
   end
 
   def destroy
     @item.destroy
-    @shelf = Shelf.find(params[:shelf_id])
-    redirect_to shelf_path(@shelf)
+    if params[:shelf_id].present?
+      @shelf = Shelf.find(params[:shelf_id])
+      redirect_to shelf_path(@shelf)
+    elsif params[:space_id].present?
+      @space = Space.find(params[:space_id])
+      redirect_to space_path(@space)
+    end
   end
 
   private
