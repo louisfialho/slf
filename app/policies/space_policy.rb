f7a03a82a257eb7209005a89bd1c4b311cbee971
Louis Fialho <louis.fialho@hec.edu>
2020-12-27 14:22:41 +0000

    implemented authorization to update or destroy a space within a space

--- app/policies/space_policy.rb
+++ app/policies/space_policy.rb
@@ -20,12 +20,16 @@ class SpacePolicy < ApplicationPolicy
       record.shelves.first.users.first == user #assuming a space can be in one shelf and one shelf can belong to one user
     # - record: the restaurant passed to the `authorize` method in controller
     # - user:   the `current_user` signed in with Devise.
+    elsif !record.connections.first.parent.nil?
+      record.connections.first.root.space.shelves.first.users.first == user #assuming a child space can have only one parent space, a parent space can have only one shelf, a shelf can belong only to one user.
     end
   end
 
   def destroy?
     if !record.shelves.empty?
       record.shelves.first.users.first == user
+    elsif !record.connections.first.parent.nil?
+      record.connections.first.root.space.shelves.first.users.first == user #assuming a child space can have only one parent space, a parent space can have only one shelf, a shelf can belong only to one user.
     end
   end
 end
