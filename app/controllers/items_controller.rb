0548a0abd8f3092b132b23c11eb1b04fd0872a9a
Louis Fialho <louis.fialho@hec.edu>
2021-01-21 16:48:08 +0100

    added status to item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,7 +57,7 @@ before_action :set_shelf_space, only: [:new, :show, :edit]
   private
 
   def item_params
-    params.require(:item).permit(:url, :medium, :name, :description, :rating)
+    params.require(:item).permit(:url, :medium, :status, :name, :description, :rating)
   end
 
   def set_item
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_122115) do
+ActiveRecord::Schema.define(version: 2021_01_21_153934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_122115) do
     t.string "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "items_shelves", id: false, force: :cascade do |t|
