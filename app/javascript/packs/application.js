169c64ea69a495921cc4e762d0fcdb91a28cb35d
Louis Fialho <louis.fialho@hec.edu>
2021-01-24 20:15:51 +0100

    added js for + button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,22 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
+const displayAddBtns = () => {
+  const plus = document.getElementById("plus");
+  const btns = document.querySelector(".btns");
+  plus.addEventListener("click", (event) => {
+    if (btns.style.display === "none") {
+      btns.style.display = "";
+    } else {
+      btns.style.display = "none";
+    }
+    plus.classList.toggle("fa-rotate-45");
+});
+}
+
+displayAddBtns();
+
+
+//export { initUpdateNavbarOnScroll };
