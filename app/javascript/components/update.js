57fd77d1a507af6914a4869a1424fd4d2bfb2677
Louis Fialho <louis.fialho@hec.edu>
2021-02-03 10:19:29 +0100

    sovled js logic of clicking anywhere

--- app/javascript/components/update.js
+++ app/javascript/components/update.js
@@ -3,6 +3,7 @@ const updateStatus = () => {
   const upRank = document.getElementById('update-rank');
   const optStat = document.getElementById('opts-status');
   const optRank = document.getElementById('opts-rank');
+
   upStat.addEventListener("click", (event) => {
     if (optStat.style.display === "none") {
       optStat.style.display = "";
@@ -10,6 +11,7 @@ const updateStatus = () => {
       optStat.style.display = "none";
     }
   });
+
   upRank.addEventListener("click", (event) => {
     if (optRank.style.display === "none") {
       optRank.style.display = "";
@@ -17,6 +19,30 @@ const updateStatus = () => {
       optRank.style.display = "none";
     }
   });
+
+document.addEventListener('click', function(event) {
+  var isClickInsideStat = optStat.contains(event.target) ||  upStat.contains(event.target);
+
+  if (!isClickInsideStat) {
+    if (optStat.style.display === "") {
+      optStat.style.display = "none";
+    }
+  }
+});
+
+document.addEventListener('click', function(event) {
+  var isClickInsideRank = optRank.contains(event.target) ||  upRank.contains(event.target);
+
+  if (!isClickInsideRank) {
+    if (optRank.style.display === "") {
+      optRank.style.display = "none";
+    }
+  }
+});
+
 }
+
+
+
 
 export { updateStatus };
