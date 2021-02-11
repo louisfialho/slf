ad5783abf1ed30d53f301834577987b956ce7241
Louis Fialho <louis.fialho@hec.edu>
2021-02-11 19:50:55 +0100

    added condition to manage empty messages

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -66,59 +66,61 @@ end
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    if message.text == '/start' || message.text.include?('/start ')
-      unique_code = message.text.split[1] # add condition if exists
-      #si il y a unique_code
-      if unique_code.to_s.strip.empty? == false
-      #  si ce unique_code correspond à un user
-        if User.find_by(telegram_hash: unique_code).nil? == false
-      #  ajouter message.chat.id à ce user
-          user = User.find_by(telegram_hash: unique_code)
-          user.telegram_chat_id = message.chat.id
-          user.save
-          bot.api.send_message(chat_id: message.chat.id, text: "Hi #{message.from.first_name}, send me a URL and I'll add the object to your shelf!")
-      #  si ce unique code ne correspond pas à un user
+    if message.text
+      if message.text == '/start' || message.text.include?('/start ')
+        unique_code = message.text.split[1] # add condition if exists
+        #si il y a unique_code
+        if unique_code.to_s.strip.empty? == false
+        #  si ce unique_code correspond à un user
+          if User.find_by(telegram_hash: unique_code).nil? == false
+        #  ajouter message.chat.id à ce user
+            user = User.find_by(telegram_hash: unique_code)
+            user.telegram_chat_id = message.chat.id
+            user.save
+            bot.api.send_message(chat_id: message.chat.id, text: "Hi #{message.from.first_name}, send me a URL and I'll add the object to your shelf!")
+        #  si ce unique code ne correspond pas à un user
+          else
+        #  je ne sais pas qui vous êtes...
+            bot.api.send_message(chat_id: message.chat.id, text: "Sorry #{message.from.first_name}, I cannot find you.")
+          end
+        #si il n'y a pas unique_code
         else
-      #  je ne sais pas qui vous êtes...
-          bot.api.send_message(chat_id: message.chat.id, text: "Sorry #{message.from.first_name}, I cannot find you.")
+          #demander de se connecter avec un lien envoyé par mail pr identifier
+          bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, please use the link sent by mail to open your first discussion with Shelf bot")
         end
-      #si il n'y a pas unique_code
       else
-        #demander de se connecter avec un lien envoyé par mail pr identifier
-        bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, please use the link sent by mail to open your first discussion with Shelf bot")
-      end
-    else
-      case message.text
-        when '/stop'
-          bot.api.send_message(chat_id: message.chat.id, text: "Ciao #{message.text}")
-        else
-          if message.text.to_s.downcase.include? "http"
-            candidate = URI.extract(message.text.to_s, ['http', 'https']).first
-            if uri?(candidate)
-              if is_redirect?(candidate)
-                fin_url = final_url(candidate)
+        case message.text
+          when '/stop'
+            bot.api.send_message(chat_id: message.chat.id, text: "Ciao #{message.text}")
+          else
+            if message.text.to_s.downcase.include? "http"
+              candidate = URI.extract(message.text.to_s, ['http', 'https']).first
+              if uri?(candidate)
+                if is_redirect?(candidate)
+                  fin_url = final_url(candidate)
+                else
+                  fin_url = candidate
+                end
+                if working_url?(fin_url)
+                  url = URI.extract(fin_url).first
+                  item_name = item_name(url)
+                  item_medium = item_medium(url)
+                  user = User.find_by(telegram_chat_id: message.chat.id)
+                  shelf = user.shelves.first
+                  item = Item.new(url: url, medium: item_medium, name: item_name, status: 'not started', rank: 'medium')
+                  shelf.items << item
+                  bot.api.send_message(chat_id: message.chat.id, text: "#{item_name} was added to your shelf! Check it out! https://www.shelf.so/items/#{item.id}?shelf_id=#{shelf.id}")
+                else
+                  bot.api.send_message(chat_id: message.chat.id, text: "Mmh... This URL doesn't seem to be valid! Please only send me valid URLs 💆‍♂️")
+                end
               else
-                fin_url = candidate
-              end
-              if working_url?(fin_url)
-                url = URI.extract(fin_url).first
-                item_name = item_name(url)
-                item_medium = item_medium(url)
-                user = User.find_by(telegram_chat_id: message.chat.id)
-                shelf = user.shelves.first
-                item = Item.new(url: url, medium: item_medium, name: item_name, status: 'not started', rank: 'medium')
-                shelf.items << item
-                bot.api.send_message(chat_id: message.chat.id, text: "#{item_name} was added to your shelf! Check it out! https://www.shelf.so/items/#{item.id}?shelf_id=#{shelf.id}")
-              else
-                bot.api.send_message(chat_id: message.chat.id, text: "Mmh... This URL doesn't seem to be valid! Please only send me valid URLs 💆‍♂️")
+                bot.api.send_message(chat_id: message.chat.id, text: "Sorry #{message.from.first_name}, but I don't understand what you are saying... Please only send me plain URLs so that I can add the corresponding object to your Shelf! 💆‍♂️")
               end
             else
               bot.api.send_message(chat_id: message.chat.id, text: "Sorry #{message.from.first_name}, but I don't understand what you are saying... Please only send me plain URLs so that I can add the corresponding object to your Shelf! 💆‍♂️")
             end
-          else
-            bot.api.send_message(chat_id: message.chat.id, text: "Sorry #{message.from.first_name}, but I don't understand what you are saying... Please only send me plain URLs so that I can add the corresponding object to your Shelf! 💆‍♂️")
-          end
         end
+      end
     end
   end
 end
