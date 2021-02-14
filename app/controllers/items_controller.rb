51dd7780956edce2595a83663e792394652ea2da
Louis Fialho <louis.fialho@hec.edu>
2021-02-14 13:36:16 +0100

    created recursive function for n-level space search

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -59,6 +59,32 @@ before_action :set_shelf_space, only: [:new, :show, :edit]
     end
   end
 
+  def move_to_space_list
+    $spaces = []
+
+    shelf = @current_user.shelves.first
+    spaces_on_shelf = shelf.spaces
+
+    for s in spaces_on_shelf do
+      connections = s.connections
+      recursive_space_search(connections)
+    end
+
+    return $spaces
+  end
+
+  def recursive_space_search(array_of_connections)
+    for c in array_of_connections do
+      $spaces << c.space.name
+      if c.space.children.empty? == false
+        children = c.space.children
+        recursive_space_search(children)
+      end
+    end
+  end
+
+  helper_method :move_to_space_list
+
   private
 
   def item_params
