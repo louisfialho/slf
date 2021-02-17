00e12ec57d5cd28b925e99db94c28df96a72d3e9
Louis Fialho <louis.fialho@hec.edu>
2021-02-17 13:39:54 +0100

    fe

--- app/policies/space_policy.rb
+++ app/policies/space_policy.rb
@@ -30,7 +30,11 @@ class SpacePolicy < ApplicationPolicy
   end
 
   def destroy?
-
+    if !record.shelves.empty?
+      record.shelves.first.user == user
+    elsif !record.connections.first.parent.nil?
+      record.connections.first.root.space.shelves.first.user == user #assuming a child space can have only one parent space, a parent space can have only one shelf, a shelf can belong only to one user.
+    end
   end
 
   def move_space_to_space?
