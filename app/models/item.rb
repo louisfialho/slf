94419c31575ed00f23b63442fb0bad791183e592
Louis Fialho <louis.fialho@hec.edu>
2021-02-25 10:24:10 +0100

    k

--- app/models/item.rb
+++ app/models/item.rb
@@ -56,11 +56,12 @@ class Item < ApplicationRecord
       html_file = URI.open(url)
       html_doc = Nokogiri::HTML(html_file)
       if url.include? 'www.youtube'
-        return html_doc.at('meta[name="title"]')['content'] # works for YouTube
+        item_name = html_doc.at('meta[name="title"]')['content'] # works for YouTube
       else
-        return html_doc.css('head title').inner_text # works for spotify and more
+        item_name = html_doc.css('head title').inner_text # works for spotify and more
       # does not work for Techcrunch
       end
+      return item_name.split.map(&:capitalize).join(' ') # caps each word
     rescue
       "No name found"
     end
