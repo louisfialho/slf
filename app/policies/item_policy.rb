eeef3e230942b3f82e001469c8e43fe465b7bc51
Louis Fialho <louis.fialho@hec.edu>
2021-03-02 15:41:26 +0100

    k

--- app/policies/item_policy.rb
+++ app/policies/item_policy.rb
@@ -15,7 +15,7 @@ class ItemPolicy < ApplicationPolicy
     elsif @parent
       if !@parent.shelves.empty?
         @parent.shelves.first.user == user
-      elsif !@parent.connections.first.root.space.shelves.first.user.empty?
+      elsif !@parent.connections.first.root.space.shelves.first.empty?
         @parent.connections.first.root.space.shelves.first.user == user
       end
     end
