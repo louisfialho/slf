e6f828ef4e62dacd8c9c2a4024a030fe04ece4a1
Louis Fialho <louis.fialho@hec.edu>
2021-04-21 20:35:25 +0200

    k

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -17,7 +17,7 @@ export default class extends Controller {
     this.sortable = Sortable.create(this.element, {
       animation: 500,
       onEnd: this.end.bind(this)
-      delay: 500, // time in milliseconds to define when the sorting should start
+      delay: 250, // time in milliseconds to define when the sorting should start
       delayOnTouchOnly: true, // only delay if user is using touch
     })
   }
