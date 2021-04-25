6913d5c63021e0744ce3873072b3b004235f1f4c
Louis Fialho <louis.fialho@hec.edu>
2021-04-25 16:05:02 +0200

    k

--- app/javascript/components/display-save.js
+++ app/javascript/components/display-save.js
@@ -1,8 +1,10 @@
 const displaySave = () => {
   const saveNotes = document.getElementById("save-notes");
   const txtArea = document.getElementById("txt-area");
+  const itemTitle = document.querySelector(".item-title");
+  const navBar = document.getElementById("navshow");
 
-  txtArea.style.height = (window.innerHeight) - 450 + "px";
+  txtArea.style.height = (window.innerHeight) - navBar.clientHeight - itemTitle.clientHeight - 200 + "px"; // define height so that it ends right beffore the bottom
 
   txtArea.addEventListener('focus', (event) => {
     saveNotes.style.display = "";
