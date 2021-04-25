c9469f4411e846e0bb0ec93a28c3e6aa0e481408
Louis Fialho <louis.fialho@hec.edu>
2021-04-25 15:57:27 +0200

    k

--- app/javascript/components/display-save.js
+++ app/javascript/components/display-save.js
@@ -2,6 +2,8 @@ const displaySave = () => {
   const saveNotes = document.getElementById("save-notes");
   const txtArea = document.getElementById("txt-area");
 
+  txtArea.style.height = (window.innerHeight) - 450 + "px";
+
   txtArea.addEventListener('focus', (event) => {
     saveNotes.style.display = "";
   });
