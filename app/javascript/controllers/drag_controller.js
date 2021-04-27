2a4e53afb5b91beafdc9c5a006df114fd2db6912
Louis Fialho <louis.fialho@hec.edu>
2021-04-27 10:55:58 +0200

    k

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -2,10 +2,6 @@
 // https://stimulusjs.org/handbook/introduction
 //
 // This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
 
 import { Controller } from "stimulus"
 import Sortable from 'sortablejs';
