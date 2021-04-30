14c58f1222a1459065858a2498ef93bdb2dbda19
Louis Fialho <louis.fialho@hec.edu>
2021-04-30 09:45:38 +0200

    k

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,13 +2,15 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Save, organize and annotate <span style='white-space:nowrap;'>resources 📚</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>blog posts 📄</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>news articles 📰</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>newsletters 🗞</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>books 📕</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>audio books 🎧</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>research papers 📃</span>"
+    strings: ["Save, organize and annotate <span style='white-space:nowrap;'>resources &#128218;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>blog posts &#128196;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>news articles &#128240;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>newsletters &#128478;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>podcasts &#127897;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>videos &#128250;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>books &#128213;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>audio books &#127911;</span>"
+    , "Save, organize and annotate <span style='white-space:nowrap;'>research papers &#128195;</span>"
     ],
     typeSpeed: 50,
     loop: true
