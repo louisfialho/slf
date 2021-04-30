b9499f43231cf88940ed58de0084434c5275dea6
Louis Fialho <louis.fialho@hec.edu>
2021-04-30 09:48:58 +0200

    k

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,15 +2,15 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Save, organize and annotate <span style='white-space:nowrap;'>resources &#128218;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>blog posts &#128196;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>news articles &#128240;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>newsletters &#128478;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>podcasts &#127897;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>videos &#128250;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>books &#128213;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>audio books &#127911;</span>"
-    , "Save, organize and annotate <span style='white-space:nowrap;'>research papers &#128195;</span>"
+    strings: ["Save, organize and annotate resources &#128218;</span>"
+    , "Save, organize and annotate blog posts &#128196;</span>"
+    , "Save, organize and annotate news articles &#128240;</span>"
+    , "Save, organize and annotate newsletters &#128478;</span>"
+    , "Save, organize and annotate podcasts &#127897;</span>"
+    , "Save, organize and annotate videos &#128250;</span>"
+    , "Save, organize and annotate books &#128213;</span>"
+    , "Save, organize and annotate audio books &#127911;</span>"
+    , "Save, organize and annotate research papers &#128195;</span>"
     ],
     typeSpeed: 50,
     loop: true
