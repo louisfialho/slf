8c86db0975a63bbdbe95d3ab39185b85d0b9aa05
Louis Fialho <louis.fialho@hec.edu>
2021-04-30 11:21:40 +0200

    k

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,20 +1,40 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Save, organize and annotate resources &#128218;</span>"
-    , "Save, organize and annotate blog posts &#128196;</span>"
-    , "Save, organize and annotate news articles &#128240;</span>"
-    , "Save, organize and annotate newsletters &#128478;</span>"
-    , "Save, organize and annotate podcasts &#127897;</span>"
-    , "Save, organize and annotate videos &#128250;</span>"
-    , "Save, organize and annotate books &#128213;</span>"
-    , "Save, organize and annotate audio books &#127911;</span>"
-    , "Save, organize and annotate research papers &#128195;</span>"
-    ],
-    typeSpeed: 50,
-    loop: true
-  });
+  if (window.matchMedia("(max-width: 610px)").matches) {
+    new Typed('#banner-typed-text', {
+      strings: ["Save, organize and <br> annotate your <br> resources &#128218;"
+      , "Save, organize and <br> annotate your <br> blog posts &#128196;"
+      , "Save, organize and <br> annotate your <br> news articles &#128240;"
+      , "Save, organize and <br> annotate your <br> newsletters &#128478;"
+      , "Save, organize and <br> annotate your <br> podcasts &#127897;"
+      , "Save, organize and <br> annotate your <br> videos &#128250;"
+      , "Save, organize and <br> annotate your <br> books &#128213;"
+      , "Save, organize and <br> annotate your <br> audio books &#127911;"
+      , "Save, organize and <br> annotate your <br> research papers &#128195;"
+      ],
+      typeSpeed: 50,
+      loop: true
+    });
+  } else {
+    new Typed('#banner-typed-text', {
+      strings: ["Save, organize and annotate resources &#128218;"
+      , "Save, organize and annotate blog posts &#128196;"
+      , "Save, organize and annotate news articles &#128240;"
+      , "Save, organize and annotate newsletters &#128478;"
+      , "Save, organize and annotate podcasts &#127897;"
+      , "Save, organize and annotate videos &#128250;"
+      , "Save, organize and annotate books &#128213;"
+      , "Save, organize and annotate audio books &#127911;"
+      , "Save, organize and annotate research papers &#128195;"
+      ],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
+
+
+
