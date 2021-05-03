0e97bd3a22cc869ca62f53a04af826e1a99f0ea4
Louis Fialho <louis.fialho@hec.edu>
2021-05-03 10:42:51 +0200

    k

---
+++ app/services/twilio_client.rb
@@ -0,0 +1,25 @@
+class TwilioClient
+  attr_reader :client
+
+  def initialize
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def send_text(dest_phone_num, message)
+    client.messages.create( #.api.account
+      to: dest_phone_num,
+      from: '+15035494537',
+      body: message
+    )
+  end
+
+  private
+
+    def account_sid
+      ENV['TWILIO_ACCOUNT_SID']
+    end
+
+    def auth_token
+      ENV['TWILIO_AUTH_TOKEN']
+    end
+end
