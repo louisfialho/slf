730ba523e42264d8d49a50353d54e6981c5037aa
Louis Fialho <louis.fialho@hec.edu>
2021-05-07 20:14:29 +0200

    k

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -16,62 +16,21 @@ const loadDynamicBannerText = () => {
       loop: true
     });
   } else {
+    var tagline = document.getElementById('tagline');
+    tagline.innerHTML = "Save, organize and annotate <br> <span id='banner-typed-text'></span>";
     new Typed('#banner-typed-text', {
-      strings: ["Save, organize and annotate resources &#128218;"
-      , "Save, organize and annotate blog posts &#128196;"
-      , "Save, organize and annotate news articles &#128240;"
-      , "Save, organize and annotate newsletters &#128478;"
-      , "Save, organize and annotate podcasts &#127897;"
-      , "Save, organize and annotate videos &#128250;"
-      , "Save, organize and annotate books &#128213;"
-      , "Save, organize and annotate audio books &#127911;"
+      strings: ["blog posts &#128196;"
+      , "news articles &#128240;"
+      , "newsletters &#128478;"
+      , "podcasts &#127897;"
+      , "videos &#128250;"
+      , "books &#128213;"
+      , "audio books &#127911;"
       ],
       typeSpeed: 50,
       loop: true
     });
   }
-
-//   new Typed('#benefit-1-typed-text', {
-//         strings: ["1. <u>Save</u> your resources &#128218;"
-//         , "1. <u>Save</u> your blog posts &#128196;"
-//         , "1. <u>Save</u> your news articles &#128240;"
-//         , "1. <u>Save</u> your newsletters &#128478;"
-//         , "1. <u>Save</u> your podcasts &#127897;"
-//         , "1. <u>Save</u> your videos &#128250;"
-//         , "1. <u>Save</u> your books &#128213;"
-//         , "1. <u>Save</u> your audio books &#127911;"
-//         ],
-//         typeSpeed: 50,
-//         loop: true
-//       });
-
-//   new Typed('#benefit-2-typed-text', {
-//         strings: ["2. <u>Organize</u> your resources &#128218;"
-//         , "2. <u>Organize</u> your blog posts &#128196;"
-//         , "2. <u>Organize</u> your news articles &#128240;"
-//         , "2. <u>Organize</u> your newsletters &#128478;"
-//         , "2. <u>Organize</u> your podcasts &#127897;"
-//         , "2. <u>Organize</u> your videos &#128250;"
-//         , "2. <u>Organize</u> your books &#128213;"
-//         , "2. <u>Organize</u> your audio books &#127911;"
-//         ],
-//         typeSpeed: 50,
-//         loop: true
-//       });
-
-//   new Typed('#benefit-3-typed-text', {
-//         strings: ["3. <u>Annotate</u> your resources &#128218;"
-//         , "3. <u>Annotate</u> your blog posts &#128196;"
-//         , "3. <u>Annotate</u> your news articles &#128240;"
-//         , "3. <u>Annotate</u> your newsletters &#128478;"
-//         , "3. <u>Annotate</u> your podcasts &#127897;"
-//         , "3. <u>Annotate</u> your videos &#128250;"
-//         , "3. <u>Annotate</u> your books &#128213;"
-//         , "3. <u>Annotate</u> your audio books &#127911;"
-//         ],
-//         typeSpeed: 50,
-//         loop: true
-//       });
 }
 
 
